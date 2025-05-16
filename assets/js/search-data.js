9cf3d433be1e5c980d51eb6b862fa883c3ad8ebe
zhaoyiww <zhaoyi.wang@geod.baug.ethz.ch>
2025-05-16 19:15:25 +0000

    Deploying to gh-pages from @ zhaoyiww/zhaoyiww.github.io@88cfc0bf06c7400112d95b356481c6156c5c45c4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,14 +39,14 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
