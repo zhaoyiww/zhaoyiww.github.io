9d0a56ebc8bf27a3d652e104a7b17d6fbfdf9820
zhaoyiww <zhaoyi.wang@geod.baug.ethz.ch>
2025-05-17 00:11:01 +0000

    Deploying to gh-pages from @ zhaoyiww/zhaoyiww.github.io@2a63499a234c3a9de629583d6b5dc7f51dcc4a90 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-repositories",
-          title: "Repositories",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-cv",
           title: "CV",
           description: "",
