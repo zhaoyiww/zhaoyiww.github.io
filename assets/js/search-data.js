a2724ee119ae5b25970cfc9be774238e0e08949d
zhaoyiww <zhaoyi.wang@geod.baug.ethz.ch>
2025-05-18 12:01:15 +0000

    Deploying to gh-pages from @ zhaoyiww/zhaoyiww.github.io@66e3c8215a53167c94aaeffd407dea2b9d0e2a1a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -55,17 +55,6 @@ ninja.data = [{
             window.location.href = "/blog/2024/3dgs/";
           
         },
-      },{id: "post-a-post-with-math",
-        
-          title: "a post with math",
-        
-        description: "an example of a blog post with some math",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2015/math/";
-          
-        },
       },{id: "news-i-participated-in-igp-doctoral-day-with-a-poster-presentation",
           title: 'I participated in IGP Doctoral Day with a poster presentation.',
           description: "",
